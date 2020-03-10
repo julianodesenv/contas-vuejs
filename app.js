d7204f533dc77eec17ec59be8d18d1bb4fb1f0c6
julianodesenv <julianodesenv@gmail.com>
2020-03-10 12:01:33 -0300

    adicionando conta

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var app = new Vue({
     el: "#app",
     data: {
+        test: '',
         title: "Contas a pagar",
         menus: [
             {id: 0, name: "Listar contas"},
             {id: 1, name: "Criar conta"}
         ],
-        activedView: 0,
+        activedView: 1,
+        bill: {
+            date_due: '',
+            name: '',
+            value: 0,
+            done: 0
+        },
+        names: [
+            'Conta de luz',
+            'Conta de água',
+            'Conta de telefone',
+            'Supermercado',
+            'Empréstimo',
+            'Gasolina'
+        ],
         bills: [
             {date_due: '20/20/2020', name: 'Conta de luz', value: 130.99, done: 1},
             {date_due: '20/20/2020', name: 'Conta de água', value: 70, done: 1},
@@ -30,8 +45,17 @@ var app = new Vue({
     methods: {
         showView: function (id) {
             this.activedView = id;
+        },
+        submit: function () {
+            this.bills.push(this.bill);
+            this.activedView = 0;
         }
     }
+});
+
+app.$watch('test', function (novoValor, velhoValor) {
+    console.log(velhoValor);
+    console.log(novoValor);
 });
 
 
