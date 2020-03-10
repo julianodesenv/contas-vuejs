0acf79ad9b67131a20f984158588ac65b1ac7c26
julianodesenv <julianodesenv@gmail.com>
2020-03-10 13:22:42 -0300

    adicionando delete

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+Vue.filter('doneLabel', function (value) {
+    if (value == 0) {
+        return "Não paga";
+    } else {
+        return "Paga";
+    }
+});
+
+Vue.filter('statusGeneral', function (value) {
+    if (value === false) {
+        return "Nenhuma conta cadastrada";
+    }
+
+    if (!value) {
+        return "Nenhuma conta a pagar";
+    } else {
+        return "Existem " + value + " conta(s) a serem pagas";
+    }
+
+});
+
 var app = new Vue({
     el: "#app",
     data: {
@@ -7,13 +28,13 @@ var app = new Vue({
             {id: 0, name: "Listar contas"},
             {id: 1, name: "Criar conta"}
         ],
-        activedView: 1,
+        activedView: 0,
         formType: 'insert',
         bill: {
             date_due: '',
             name: '',
             value: 0,
-            done: 0
+            done: false
         },
         names: [
             'Conta de luz',
@@ -24,23 +45,27 @@ var app = new Vue({
             'Gasolina'
         ],
         bills: [
-            {date_due: '20/20/2020', name: 'Conta de luz', value: 130.99, done: 1},
-            {date_due: '20/20/2020', name: 'Conta de água', value: 70, done: 1},
-            {date_due: '20/20/2020', name: 'Conta de telefone', value: 140, done: 0},
-            {date_due: '20/20/2020', name: 'Supermercado', value: 240.35, done: 0},
-            {date_due: '20/20/2020', name: 'Empréstimo', value: 0, done: 0},
-            {date_due: '20/20/2020', name: 'Gasolina', value: 250.99, done: 0}
+            {date_due: '20/20/2020', name: 'Conta de luz', value: 130.99, done: true},
+            {date_due: '20/20/2020', name: 'Conta de água', value: 70, done: true},
+            {date_due: '20/20/2020', name: 'Conta de telefone', value: 140, done: false},
+            {date_due: '20/20/2020', name: 'Supermercado', value: 240.35, done: false},
+            {date_due: '20/20/2020', name: 'Empréstimo', value: 0, done: false},
+            {date_due: '20/20/2020', name: 'Gasolina', value: 250.99, done: false}
         ]
     },
     computed: {
         status: function () {
+            if (!this.bills.length) {
+                return false;
+            }
+
             var count = 0;
             for (var i in this.bills) {
                 if (!this.bills[i].done) {
                     count++;
                 }
             }
-            return !count ? "Nehuma conta a pagar" : "Existem " + count + " a serem pagas";
+            return count;
         }
     },
     methods: {
@@ -59,7 +84,7 @@ var app = new Vue({
                 date_due: '',
                 name: '',
                 value: 0,
-                done: 0
+                done: false
             };
 
             this.activedView = 0;
@@ -68,15 +93,12 @@ var app = new Vue({
             this.bill = bill;
             this.activedView = 1;
             this.formType = 'update';
+        },
+        deleteBill: function (bill) {
+            if (confirm('Deseja excluir?')) {
+                this.bills.$remove(bill);
+            }
         }
-    }
-});
-
-Vue.filter('doneLabel', function (value) {
-    if (value == 0) {
-        return "Não paga";
-    } else {
-        return "Paga";
     }
 });
 
